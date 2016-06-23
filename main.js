626b022b4cf8c56aac3f6e2b47b76173b16db0c6
jenny kim <ms.jennykimmy@gmail.com>
2016-06-22 17:33:33 -0700

    saved classwork

---
+++ main.js
@@ -0,0 +1,23 @@
+var wdi_24_names = ["Adrianna", "Andrew", "Christian", "Daniel", "David", "Desi", "Emily", "Jenny", "Kate", "Kevin", "Matt", "Michael", "Ramin", "Taylor"];
+
+// for (i=0; i < wdi_24_names.length; i++) {
+//   console.log(wdi_24_names[i]);
+// }
+
+//
+// var i = 0;
+// function eachName() {
+//   console.log(wdi_24_names[i]);
+//   i++
+// }
+
+var randomName = wdi_24_names[Math.floor(Math.random() * wdi_24_names.length)];
+console.log(randomName);
+
+// var name = Math.floor((Math.random() * wdi_24_names.length));
+// var upToEndOfArray = random *wdi_24_names.length;
+// var integer = upToEndOfArray.floor(upToEndOfArray);
+// console.log(wdi_24_names[integer]);
+
+// var integer = Math.floor(Math.random() * wdi_24_names.length);
+// console.log(wdi_24_names[integer]);
